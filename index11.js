3512a55ca17c3c3421a2347c140116e89aa512ac
Imaran-khan <imrankhan958585@gmail.com>
2025-07-27 21:12:43 +0530

    add

---
+++ index11.js
@@ -0,0 +1,18 @@
+
+var age = 25;
+
+
+function displayAge() {
+  console.log("Age (inside displayAge):", age);
+}
+
+
+function changeAge() {
+  age = 30; 
+  console.log("Age (inside changeAge):", age);
+}
+
+
+displayAge();  
+changeAge();   
+displayAge(); 
