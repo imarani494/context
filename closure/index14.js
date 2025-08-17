0777dd55cc14c9d4c9be7fb375082493db031b6f
Imaran-khan <imrankhan958585@gmail.com>
2025-08-17 14:21:14 +0530

    web

--- closure/index14.js
+++ closure/index14.js
@@ -12,8 +12,8 @@ function createFunctionList() {
 
 const functionList = createFunctionList();
 
-functionList[0](); // Output: Index: 0
-functionList[1](); // Output: Index: 1
-functionList[2](); // Output: Index: 2
-functionList[3](); // Output: Index: 3
-functionList[4](); // Output: Index: 4
+functionList[0](); 
+functionList[1](); 
+functionList[2](); 
+functionList[3](); 
+functionList[4](); 
